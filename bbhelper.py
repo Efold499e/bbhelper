4caaafbebdf8c0582184c158ccaf6da29dd155d9
hyz <yz.hong@foxmail.com>
2025-04-23 11:11:10 +0800

    Initial Commit

---
+++ bbhelper.py
@@ -0,0 +1,104 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import json
+from getpass import getpass
+
+class bbhelper:
+    id = 0
+    password = ""
+    driver = webdriver.Chrome()
+    titles = ["作业", "ssignment", "omework"]
+    def __init__(self, id, password, titles):
+        self.id = id
+        self.password = password
+        self.titles = titles
+        self.driver.implicitly_wait(3)
+        self.driver.get('https://cas.sustech.edu.cn/cas/login?service=https://bb.sustech.edu.cn/webapps/bb-sso-BBLEARN/index.jsp')
+        time.sleep(0.5)
+        self.driver.find_element(By.CSS_SELECTOR, '#username').send_keys(self.id)
+        self.driver.find_element(By.CSS_SELECTOR, '#password').send_keys(self.password)
+        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit'][accesskey='l']").click()
+        button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[id='agree_button']")))
+        button.click()
+    def get_homework(self):
+        homework = self.driver.find_element(By.CSS_SELECTOR, '#content_listContainer').text
+        print(homework +'\n')
+        self.driver.back()
+        return homework
+    def roll_course(self):
+        homework_dict = {}
+        courses = self.driver.find_elements(By.CSS_SELECTOR,
+            "a[href*='/webapps/blackboard/execute/launcher?type=Course'][target='_top']")
+        len_courses = len(courses)
+        for i in range(len_courses):
+            time.sleep(1)
+            courses = self.driver.find_elements(By.CSS_SELECTOR,
+            "a[href*='/webapps/blackboard/execute/launcher?type=Course'][target='_top']")
+            course = courses[i]
+            course.click()
+            time.sleep(1)
+            self.driver.find_element(By.XPATH, "//*[@id='menuPuller']").click()
+            titles = self.titles
+            course_name = self.driver.find_element(By.CSS_SELECTOR,"#courseMenu_link").text
+            print(f"正在获取 {course_name}的作业喵~\n")
+            has_homework = 0
+            for title in titles:
+                homework_list = self.driver.find_elements(By.CSS_SELECTOR, f"span[title*='{title}']")
+                time.sleep(0.5)
+                self.driver.find_element(By.XPATH, "//*[@id='menuPuller']").click()
+                if homework_list != []:
+                    has_homework = 1
+                    if len(homework_list) != 1:
+                        j = 0
+                        for j in range(len(homework_list)):
+                            self.driver.find_element(By.XPATH, "//*[@id='menuPuller']").click()
+                            homework_list = self.driver.find_elements(By.CSS_SELECTOR, f"span[title*='{title}']")
+                            homework_list[j].click()
+                            homework = self.get_homework()
+                            homework_dict[course_name] = homework
+                    else:
+                        self.driver.find_element(By.XPATH, "//*[@id='menuPuller']").click()
+                        homework = homework_list[0].click() 
+                        homework = self.get_homework()
+                        homework_dict[course_name] = homework
+                else:
+                    continue
+            if has_homework == 0:
+                print(f"没有作业喵~\n")
+            self.driver.back()
+        return homework_dict
+    def write_homework(self, homework_dict):
+        for course, homework in homework_dict.items():
+            current_time = time.strftime("%Y-%m-%d", time.localtime())
+            os.makedirs(f"./homework/{current_time}", exist_ok=True)
+            with open(f"./homework/{current_time}/{course}_{current_time}.txt", "w", encoding="utf-8") as f:
+                f.write(homework)
+                print(f"已将{course}的作业写入文件{current_time}_{course}.txt喵~\n")
+        print("作业在homework文件夹下喵~\n")
+
+    def close(self):
+        print("获取作业完成，正在退出浏览器喵~")
+        self.driver.quit()
+
+if __name__ == '__main__':
+    login = json.load(open("login.json", "r", encoding="utf-8"))
+    if login["id"] == "<Your SUSTech id>" or login["password"] == "<Your password>":
+        print("欢迎使用bbhelper，首次使用请在终端键入您的学号和密码喵~")
+        id = input("请输入您的学号：")
+        password = getpass("请输入CAS密码（密码不显示，输入完按回车即可）：")
+        json.dump({"id": id, "password": password, "titles": ["作业", "ssignment", "omework"]}, open("login.json", "w", encoding="utf-8"))
+    else:
+        print("欢迎使用bbhelper，已有登录信息，即将开始获取作业喵~")
+    titles = login["titles"]
+    id = login["id"]
+    password = login["password"]
+    bb = bbhelper(id = id,password = password , titles = titles)
+    homework_dict = bb.roll_course()
+    bb.write_homework(homework_dict)
+    bb.close()
+
---
+++ readme.md
@@ -0,0 +1,30 @@
+# bbhelper
+
+> author: Efold499e
+
+## 前言
+
+好玩就做了，新手，纯菜，写的很多地方有史，骂的时候轻一点
+
+## 有什么用
+
+通过自动化流程帮助你完成每天都要查bb作业的坐牢过程，会将当前bb上作业相关板块作业返回到终端，并在 __homework__ 文件下分学科存储
+
+- bbhelper/
+  - homework/
+  - bbhelper.py
+  - login.json
+  - requirements.txt
+  - README.md
+
+## 如何使用
+
+在终端输入
+
+```bash
+pip install -r requirements.txt
+
+python bbhelper.py
+```
+
+当然也可以用类里面的函数diy，就酱。
